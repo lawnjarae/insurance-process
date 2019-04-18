6585436928e15f34ee332207783e1e7c30becf43
pamAdmin <>
2019-04-18 14:53:30 -0600

    {/src/main/java/com/redhat/demo/insurance_process/Applicant.java}

--- src/main/java/com/redhat/demo/insurance_process/Applicant.java
+++ src/main/java/com/redhat/demo/insurance_process/Applicant.java
@@ -6,12 +6,65 @@ package com.redhat.demo.insurance_process;
 
 public class Applicant implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Applicant() {
-    }
+	private java.lang.String address;
+	private java.lang.String name;
+	private java.lang.String businessType;
+	private java.lang.Double amountRequested;
+	private java.lang.Integer creditRating;
 
+	public Applicant() {
+	}
 
+	public java.lang.String getAddress() {
+		return this.address;
+	}
 
+	public void setAddress(java.lang.String address) {
+		this.address = address;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getBusinessType() {
+		return this.businessType;
+	}
+
+	public void setBusinessType(java.lang.String businessType) {
+		this.businessType = businessType;
+	}
+
+	public java.lang.Double getAmountRequested() {
+		return this.amountRequested;
+	}
+
+	public void setAmountRequested(java.lang.Double amountRequested) {
+		this.amountRequested = amountRequested;
+	}
+
+	public java.lang.Integer getCreditRating() {
+		return this.creditRating;
+	}
+
+	public void setCreditRating(java.lang.Integer creditRating) {
+		this.creditRating = creditRating;
+	}
+
+	public Applicant(java.lang.String address, java.lang.String name,
+			java.lang.String businessType, java.lang.Double amountRequested,
+			java.lang.Integer creditRating) {
+		this.address = address;
+		this.name = name;
+		this.businessType = businessType;
+		this.amountRequested = amountRequested;
+		this.creditRating = creditRating;
+	}
 
 }
